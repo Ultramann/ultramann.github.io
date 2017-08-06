1ca27858a3042b850ef4295fc56153b29e74d30d
Ultramann <cary.goltermann@gmail.com>
2017-08-06 22:28:17 +0000

    Add draft functionality

--- src/site.hs
+++ src/site.hs
@@ -2,86 +2,91 @@
 
 import Data.Monoid ((<>))
 import Data.List (isSuffixOf)
-import System.FilePath
+import System.Environment (getArgs)
+import System.FilePath (takeBaseName)
 import Hakyll
 
 main :: IO ()
-main = hakyllWith siteConf $ do
-  match "images/*" $ do
-    route   idRoute
-    compile copyFileCompiler
+main = do
+  (arg:_) <- getArgs 
+  let posts = fromGlob $ "posts/" ++ if arg == "watch" || arg == "preview"
+                                       then "**" else "*"
+  hakyllWith siteConf $ do
+    match "images/**" $ do
+      route   idRoute
+      compile copyFileCompiler
 
-  match "css/*" $ do
-    route   idRoute
-    compile compressCssCompiler
+    match "css/*" $ do
+      route   idRoute
+      compile compressCssCompiler
 
-  match "pages/*" $ do
-    route   $ niceRoute ""
-    compile $ pandocCompiler
-          >>= loadAndApplyTemplate "templates/base.html" defaultContext
+    match "pages/*" $ do
+      route   $ niceRoute ""
+      compile $ pandocCompiler
+            >>= loadAndApplyTemplate "templates/base.html" defaultContext
+            >>= relativizeUrls
+
+    match posts $ do
+      route $ niceRoute "posts/"
+      compile $ pandocCompiler
+            >>= loadAndApplyTemplate "templates/post.html" postCtx
+            >>= saveSnapshot "teaser"
+            >>= loadAndApplyTemplate "templates/base.html" postCtx
+            >>= relativizeUrls
+
+    create ["posts.html"] $ do
+      route   $ niceRoute ""
+      compile $ do
+        posts <- recentFirst =<< loadAll posts
+        let archiveCtx = listField "posts" postCtx (return posts)
+                      <> constField "title" "Past Posts"
+                      <> defaultContext
+
+        makeItem ""
+          >>= loadAndApplyTemplate "templates/posts.html" archiveCtx
+          >>= loadAndApplyTemplate "templates/base.html"  archiveCtx
           >>= relativizeUrls
+          >>= cleanIndexUrls
 
-  match "posts/*" $ do
-    route $ niceRoute "posts/"
-    compile $ pandocCompiler
-          >>= loadAndApplyTemplate "templates/post.html" postCtx
-          >>= saveSnapshot "teaser"
-          >>= loadAndApplyTemplate "templates/base.html" postCtx
+    match "notes/*" $ do
+      route $ niceRoute "notes/"
+      compile $ pandocCompiler
+            >>= loadAndApplyTemplate "templates/post.html" postCtx
+            >>= loadAndApplyTemplate "templates/base.html" postCtx
+            >>= relativizeUrls
+
+    create ["notes.html"] $ do
+      route   $ niceRoute ""
+      compile $ do
+        notes <- recentFirst =<< loadAll "notes/*"
+        let archiveCtx = listField "posts" postCtx (return notes)
+                      <> constField "title" "Notes"
+                      <> defaultContext
+
+        makeItem ""
+          >>= loadAndApplyTemplate "templates/notes.html" archiveCtx
+          >>= loadAndApplyTemplate "templates/base.html"  archiveCtx
           >>= relativizeUrls
+          >>= cleanIndexUrls
 
-  create ["posts.html"] $ do
-    route   $ niceRoute ""
-    compile $ do
-      posts <- recentFirst =<< loadAll "posts/*"
-      let archiveCtx = listField "posts" postCtx (return posts)
-                    <> constField "title" "Past Posts"
+    match "index.html" $ do
+      route idRoute
+      compile $ do
+        posts <- recentFirst =<< loadAll posts
+        notes <- recentFirst =<< loadAll "notes/*"
+        let homePostCtx = teaserField "teaser" "teaser" <> postCtx
+            indexCtx = listField "posts" homePostCtx (return (take 1 posts))
+                    <> listField "notes" postCtx (return (take 6 notes))
+                    <> constField "title" "Mostly Literate Machine Learning"
                     <> defaultContext
 
-      makeItem ""
-        >>= loadAndApplyTemplate "templates/posts.html" archiveCtx
-        >>= loadAndApplyTemplate "templates/base.html"  archiveCtx
-        >>= relativizeUrls
-        >>= cleanIndexUrls
-
-  match "notes/*" $ do
-    route $ niceRoute "notes/"
-    compile $ pandocCompiler
-          >>= loadAndApplyTemplate "templates/post.html" postCtx
-          >>= loadAndApplyTemplate "templates/base.html" postCtx
+        getResourceBody
+          >>= applyAsTemplate indexCtx
+          >>= loadAndApplyTemplate "templates/base.html" indexCtx
           >>= relativizeUrls
+          >>= cleanIndexUrls
 
-  create ["notes.html"] $ do
-    route   $ niceRoute ""
-    compile $ do
-      notes <- recentFirst =<< loadAll "notes/*"
-      let archiveCtx = listField "posts" postCtx (return notes)
-                    <> constField "title" "Notes"
-                    <> defaultContext
-
-      makeItem ""
-        >>= loadAndApplyTemplate "templates/notes.html" archiveCtx
-        >>= loadAndApplyTemplate "templates/base.html"  archiveCtx
-        >>= relativizeUrls
-        >>= cleanIndexUrls
-
-  match "index.html" $ do
-    route idRoute
-    compile $ do
-      posts <- recentFirst =<< loadAll "posts/*"
-      notes <- recentFirst =<< loadAll "notes/*"
-      let homePostCtx = teaserField "teaser" "teaser" <> postCtx
-          indexCtx = listField "posts" homePostCtx (return (take 1 posts))
-                  <> listField "notes" postCtx (return (take 6 notes))
-                  <> constField "title" "Mostly Literate Machine Learning"
-                  <> defaultContext
-
-      getResourceBody
-        >>= applyAsTemplate indexCtx
-        >>= loadAndApplyTemplate "templates/base.html" indexCtx
-        >>= relativizeUrls
-        >>= cleanIndexUrls
-
-  match "templates/*" $ compile templateBodyCompiler
+    match "templates/*" $ compile templateBodyCompiler
 
 siteConf :: Configuration
 siteConf = defaultConfiguration { deployCommand        = "bash src/deploy.sh"
